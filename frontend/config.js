fb14effbe3df1173daf00fe262d850d97b6b8e7f
Deepanshu Gupta <dipanshugupta921@gmail.com>
2024-04-22 16:09:28 +0530

    updated api routes

--- frontend/config.js
+++ frontend/config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "https://salon-booking-ojin.vercel.app/api/v1";
+export const BASE_URL = "https://sa-backend-xfti.onrender.com/api/v1";
 export const token = localStorage.getItem("token");
