6f4078557fdcc014a39d294e7cbd54647cea35ab
Deepanshu Gupta <dipanshugupta921@gmail.com>
2024-04-22 16:54:17 +0530

    added vercel analytics

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,12 +5,14 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { Analytics } from "@vercel/analytics/react";
 import { AuthContextProvider } from "./context/AuthContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthContextProvider>
+        <Analytics />
         <ToastContainer
           theme="dark"
           position="bottom-right"
