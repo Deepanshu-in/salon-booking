e7894f6de927400e3bafb6e81ea615e873bfeea2
Deepanshu Gupta <dipanshugupta921@gmail.com>
2024-04-24 15:35:11 +0530

    added backend

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,5 @@
 // export const BASE_URL = "https://sa-backend-xfti.onrender.com/api/v1";
-export const BASE_URL = "https://sa-backend-69587c20e2df.herokuapp.com/api/v1";
+export const BASE_URL =
+  "https://salon-backend-06b19bc39279.herokuapp.com/api/v1";
 
 export const token = localStorage.getItem("token");
