b94aac44aa0ef6977a810be259ae16ae0e065fe2
Deepanshu Gupta <dipanshugupta921@gmail.com>
2024-10-24 17:43:51 +0530

    modified protected routes config

--- frontend/src/routes/ProtectedRoutes.jsx
+++ frontend/src/routes/ProtectedRoutes.jsx
@@ -1,12 +1,22 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { Navigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { authContext } from "../context/AuthContext";
 
 const ProtectedRoutes = ({ children, allowedRoles }) => {
   const { token, role } = useContext(authContext);
   const isAllowed = allowedRoles.includes(role);
+
+  useEffect(() => {
+    if (!isAllowed) {
+      toast.warning("Please log in or register to access this page");
+    }
+  }, [isAllowed]);
+
   const accessibleRoutes =
     token && isAllowed ? children : <Navigate to="/login" replace={true} />;
+
   return accessibleRoutes;
 };
 
